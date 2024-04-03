018190d613d6f50fabdc192938e8ee53f644fac3
emboucher <em.boucher@live.com>
2024-04-02 17:39:16 -0700

    started js

--- docs/app.js
+++ docs/app.js
@@ -6,3 +6,20 @@
 */
 
 const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav')
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+
+slides.forEach(setSlidePosition);
+//when i click left, move slides to the left
+// when i click right, move slides to the right
+//when i click the nav indicators, move to that slide
