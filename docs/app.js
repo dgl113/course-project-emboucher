d62355493de3de0d49bdb97ae67a02fc6601154b
emboucher <em.boucher@live.com>
2024-04-02 18:05:44 -0700

    js move to next slide

--- docs/app.js
+++ docs/app.js
@@ -21,5 +21,17 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 //when i click left, move slides to the left
+
+
 // when i click right, move slides to the right
+nextButton.addEventListener('click' , e=> {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const amountToMove = nextSlide.style.left;
+     //move to the next slide
+    track.style.transform = 'translateX(-' + amountToMove + ')';
+    currentSlide.classList.remove('current-slide');
+    nextSlide.classList.add('current-slide');
+})
+
 //when i click the nav indicators, move to that slide
